9d8c44cf5f6ce646f2749c1acfbd16eb386a3ab1
Kimiya <102611881+Kimiagoudarzi@users.noreply.github.com>
2022-07-03 20:17:14 +0430

    Q4 done

---
+++ Q4/main.js
@@ -0,0 +1,6 @@
+$("#btn-1").click(function(){
+    $("#div-1 h1").replaceWith("<h2>Welcome to the javaTponit.com</h2>");
+    $("#div-1 h3").replaceWith("<h4>It is an example of using the jQuery add() method.</h4>")
+    $(".p-1").css({"background-color":"#fffa65","border-size":"2px", "border-style":"dotted","border-color": "#EA2027"});
+    $(".s-1").css({"background-color":"#fffa65","border-size":"2px", "border-style":"dotted","border-color": "#EA2027"});
+})
